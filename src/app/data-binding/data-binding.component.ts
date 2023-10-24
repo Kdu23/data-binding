d4a73ade78ef144cd20becbba75093e59246767e
kdu23 <kdu.itech@gmail.com>
2023-10-23 23:10:25 -0300

    data-binding-aulas-Loiane-Angular

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent {
+
+url:string = 'http://loiane.traning'
+
+cursoAngular: boolean = true;
+urlImagem = 'https://loremflickr.com/320/240';
+
+
+getValor() {
+return 1
+}
+
+
+getCurtirCurso() {
+  return true
+}
+
+
+}
